390f490511bb80a2861f7648ecba965bdcd70ef5
Anna Janusz <anna.janusz@loycon.pl>
2021-09-16 18:21:52 +0200

    Add custom widget

---
+++ android/app/src/main/java/com/janua/knee_acl_mcl/CounterOperaction.kt
@@ -0,0 +1,31 @@
+package com.janua.knee_acl_mcl
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class CounterOperaction : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+    val views = RemoteViews(context.packageName, R.layout.counter_operaction)
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
